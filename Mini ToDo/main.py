b912bba0a41b295ffa71521485766569e567a9a2
Brendom Gonçalves <brendom.vi@gmail.com>
2023-06-30 14:55:01 -0300

    Alteração de cores

--- Mini ToDo/main.py
+++ Mini ToDo/main.py
@@ -1,4 +1,5 @@
 import flet as ft
+from flet import theme
 
 class Task(ft.UserControl):
     def __init__(self, input_text, remove_task):
@@ -7,21 +8,21 @@ class Task(ft.UserControl):
         self.remove_task = remove_task
 
     def build(self):
-        self.task_cb = ft.Checkbox(label=self.input, expand=True)
-        self.edit_tf = ft.TextField(value=self.input, expand=True)
+        self.task_cb = ft.Checkbox(label=self.input, expand=True, fill_color=ft.colors.YELLOW_500)
+        self.edit_tf = ft.TextField(value=self.input, expand=True, color=ft.colors.YELLOW_500, border_color=ft.colors.YELLOW_500)
         self.task_view = ft.Row(
             visible=True,
             controls=[
                 self.task_cb,
-                ft.IconButton(icon=ft.icons.CREATE_OUTLINED, on_click=self.edit_clicked),
-                ft.IconButton(icon=ft.icons.DELETE_OUTLINE, on_click=self.remove_clicked)
+                ft.IconButton(icon=ft.icons.CREATE_OUTLINED, on_click=self.edit_clicked, icon_color=ft.colors.YELLOW_500),
+                ft.IconButton(icon=ft.icons.DELETE_OUTLINE, on_click=self.remove_clicked, icon_color=ft.colors.YELLOW_500)
             ]
         )
         self.edit_view = ft.Row(
             visible=False,
             controls=[
                 self.edit_tf,
-                ft.IconButton(icon=ft.icons.CHECK, on_click=self.save_clicked)
+                ft.IconButton(icon=ft.icons.CHECK, on_click=self.save_clicked, icon_color=ft.colors.YELLOW_500)
             ]
         )
         return ft.Column(controls=[self.task_view, self.edit_view])
@@ -42,18 +43,18 @@ class Task(ft.UserControl):
 
 class ToDo(ft.UserControl):
     def build(self):
-        self.input = ft.TextField(hint_text="O que deseja deixar de lembrete?", expand=True)
+        self.input = ft.TextField(hint_text="O que deseja deixar de lembrete?", expand=True, color=ft.colors.WHITE)
         self.tasks = ft.Column()
 
         view = ft.Column(
             horizontal_alignment=ft.CrossAxisAlignment.CENTER,
             controls=[
-                ft.Text(value='ToDos',
-                        style=ft.TextThemeStyle.HEADLINE_MEDIUM),
+                ft.Text(value='Seus ToDo\'s',
+                        style=ft.TextThemeStyle.HEADLINE_MEDIUM, color=ft.colors.YELLOW_500),
                 ft.Row(
                     controls=[
                         self.input,
-                        ft.FloatingActionButton(icon=ft.icons.ADD, on_click=self.add_clicker)
+                        ft.FloatingActionButton(icon=ft.icons.ADD_ROUNDED, on_click=self.add_clicker, bgcolor=ft.colors.YELLOW_500)
                     ]
                 ),
                 self.tasks
@@ -77,6 +78,7 @@ class ToDo(ft.UserControl):
 def main(page: ft.Page):
     page.window_height = 600
     page.window_width = 400
+    page.bgcolor = ft.colors.SHADOW
 
     page.title = 'ToDo'
 
